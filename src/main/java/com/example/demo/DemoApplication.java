6170acb0eb33ad84a4c64be2e7b62071fcb61bfb
Vaishakh6358 <40035579+Vaishakh6358@users.noreply.github.com>
2021-06-14 10:27:58 -0400

    initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
